72616818ed222d986b1d3c55f09dc7d5439464c0
Sarthak Jariwala <jariwala@uw.edu>
2020-08-18 21:30:01 -0700

    scalebar properties can be set using kwargs and also through rc dict; patch version bump

--- tests/test_context.py
+++ tests/test_context.py
@@ -44,15 +44,30 @@ def test_set_image(cmap, origin, interpolation):
     assert mpl.rcParams["image.interpolation"] == interpolation
 
 
-def test_set_scalebar():
-    isns.set_scalebar()
+@pytest.mark.parametrize("color", ["white", "k", "C4"])
+@pytest.mark.parametrize("height_fraction", [0.025])
+@pytest.mark.parametrize("length_fraction", [0.3])
+@pytest.mark.parametrize("scale_loc", ["top", "bottom"])
+@pytest.mark.parametrize("location", ["upper right", "center"])
+@pytest.mark.parametrize("box_alpha", [0, 0.4])
+def test_set_scalebar(
+    color, height_fraction, length_fraction, scale_loc, location, box_alpha
+):
+    isns.set_scalebar(
+        color=color,
+        height_fraction=height_fraction,
+        length_fraction=length_fraction,
+        scale_loc=scale_loc,
+        location=location,
+        box_alpha=box_alpha,
+    )
 
-    assert mpl.rcParams["scalebar.color"] == "white"
-    assert mpl.rcParams["scalebar.height_fraction"] == 0.05
-    assert mpl.rcParams["scalebar.length_fraction"] == 0.3
-    assert mpl.rcParams["scalebar.scale_loc"] == "top"
-    assert mpl.rcParams["scalebar.location"] == "lower right"
-    assert mpl.rcParams["scalebar.box_alpha"] == 0
+    assert mpl.rcParams["scalebar.color"] == color
+    assert mpl.rcParams["scalebar.height_fraction"] == height_fraction
+    assert mpl.rcParams["scalebar.length_fraction"] == length_fraction
+    assert mpl.rcParams["scalebar.scale_loc"] == scale_loc
+    assert mpl.rcParams["scalebar.location"] == location
+    assert mpl.rcParams["scalebar.box_alpha"] == box_alpha
 
 
 def test_set_context_w_rc():
@@ -75,6 +90,7 @@ def test_scalebar_w_rc():
             "scale_loc": "bottom",
             "location": "upper right",
             "box_alpha": 1,
+            "label_loc": "top"
         }
     )
 
@@ -84,6 +100,7 @@ def test_scalebar_w_rc():
     assert mpl.rcParams["scalebar.scale_loc"] == "bottom"
     assert mpl.rcParams["scalebar.location"] == "upper right"
     assert mpl.rcParams["scalebar.box_alpha"] == 1
+    assert mpl.rcParams["scalebar.label_loc"] == "top"
 
 
 def test_rc_assertion():
