a926d0b3998a729ac86c10cb4f5a275dc6e24590
Sarthak Jariwala <jariwala@uw.edu>
2020-10-17 12:56:36 -0700

    fix imagegrid example

--- examples/plot_ImageGrid.py
+++ examples/plot_ImageGrid.py
@@ -8,7 +8,5 @@ import seaborn_image as isns
 # load images
 pol = isns.load_image("polymer")
 pl = isns.load_image("fluorescence")
-pol_out = isns.load_image("polymer outliers")
 
-g = isns.ImageGrid([pol, pl, pol_out], robust=[False, False, True], perc=[None, None, (2, 99.9)])
 g = isns.ImageGrid([pol, pl])
