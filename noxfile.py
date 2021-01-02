eadc97cd8ea704f6c6eeb1c11584c1ff72efb307
Sarthak Jariwala <jariwala@uw.edu>
2021-01-02 11:24:36 -0800

    fix hash error for new pip resolver

--- noxfile.py
+++ noxfile.py
@@ -17,6 +17,7 @@ def install_with_constraints(session, *args, **kwargs):
             "export",
             "--dev",
             "--format=requirements.txt",
+            "--without-hashes",
             f"--output={requirements}",
             external=True,
         )
