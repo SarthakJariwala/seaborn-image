ef448a457c72fc4523408f83a7e3b1ea1292b7ef
eugenioLR <eugeniolorente0@gmail.com>
2024-07-15 10:37:16 +0200

    fixed issues with documentation when using the CIFAR10 dataset

--- examples/plot_ImageGrid_3d_list.py
+++ examples/plot_ImageGrid_3d_list.py
@@ -5,6 +5,8 @@ List of 3D images
 
 import seaborn_image as isns
 
+isns.set_image(origin="lower")
+
 cifar_list = isns.load_image("cifar10 list")
 
 g = isns.ImageGrid(cifar_list, cbar=False, height=1, col_wrap=10)
--- examples/plot_ImageGrid_4d.py
+++ examples/plot_ImageGrid_4d.py
@@ -5,6 +5,8 @@
 
 import seaborn_image as isns
 
+isns.set_image(origin="lower")
+
 cifar = isns.load_image("cifar10")
 
 g = isns.ImageGrid(cifar, cbar=False, height=1, col_wrap=10)
